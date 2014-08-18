7666401fa055eb09373cf5785449ef6eedf80742
Martin Quinson <martin.quinson@loria.fr>
2014-08-18 19:28:09 +0200

    more debug info

--- app/controllers/Identity.java
+++ app/controllers/Identity.java
@@ -16,22 +16,20 @@ public class Identity extends Controller {
 	}
 
 	public static Result linkIdentity(String username, String UUID, String mail) {
-		String s;
-		
-		s = "Username : "+ username + "\nUUID : " + UUID +"\nMail : " + mail + "\n";
-		
-		System.out.println(s);
-		
+				
 		String hashUUID = hashed(UUID);
+		
+		System.out.println("Username: "+ username + 
+				"\nUUID : " + UUID + 
+				" -- hashed UUID : " + hashUUID + 
+				"\nMail : " + mail + "\n");
 		
 		Student stu = new Student(username, mail, hashUUID);
 		try {
 			stu.save();
 			stu.saveManyToManyAssociations("courses");
 		} catch(PersistenceException ex) {
-			s += "\n"
-				+ "ERROR executing DML bindLog[] error[Unique index or primary key violation: PRIMARY_KEY_B ON PUBLIC.STUDENT(UUID)"
-				+ "\n";
+			System.out.println("ERROR executing DML bindLog[] error[Unique index or primary key violation: PRIMARY_KEY_B ON PUBLIC.STUDENT(UUID)");
 		}
 		
 		return ok(
